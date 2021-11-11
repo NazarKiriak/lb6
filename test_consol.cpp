f4997486747a0ddaf643e16ce43200a3d08585d9
NazarKiriak <57nazar57@gmail.com>
2021-11-11 18:43:50 +0200

    Changed

---
+++ test_consol.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	int day = 60 * 60 * 24;
+	int week = 60 * 60 * 24 * 7;
+	int year = 60 * 60 * 24 * 7 * 52;
+	printf("Day: %i\nWeek: %i\n Year: %i\n", day, week, year);//first edit
+}
